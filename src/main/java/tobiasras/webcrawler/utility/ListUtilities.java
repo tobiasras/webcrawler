4f4087d8e848fa5af955db62c131cd9250fba0f0
tobiasras <tobiasras.job@gmail.com>
2023-02-10 00:12:49 +0100

    refactored ListUtilities for prints

--- src/main/java/tobiasras/webcrawler/utility/ListUtilities.java
+++ src/main/java/tobiasras/webcrawler/utility/ListUtilities.java
@@ -7,32 +7,6 @@ import java.util.List;
 
 public class ListUtilities<Object> {
 
-    public static void main(String[] args) {
-        ListUtilities<String> utilities = new ListUtilities<>();
-        LinkedList<String> list = new LinkedList<>();
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("2");
-        list.add("2");
-        list.add("2");
-        list.add("3");
-
-
-        var listOfList = utilities.splitList(list, 2);
-
-        for (List<String> strings : listOfList) {
-            //System.out.println(strings.size());
-
-            for (String string : strings) {
-                System.out.println(string);
-            }
-
-
-        }
-
-    }
-
     public List<List<Object>> splitList(List<Object> origin, int splitAmount) {
         List<List<Object>> listOfList = new ArrayList<>();
         int startIndex = 0;
